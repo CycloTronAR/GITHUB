95184b6ab2e245ad4f0570132fbc65c80d0ac318
Aditya Raj <adityaraj123.spn@gmail.com>
2022-10-05 13:36:57 +0530

    added mul file

---
+++ mutiply.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a,b,mul;
+    a=3;
+    b=4;
+    mul=a*b;
+    cout<<"your sum is:"<<mul;
+    return 0;
+}
